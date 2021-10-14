ecda21ddbbee46f5cafd9053dca2ad2e183a0fdd
Charming (차밍) <loadkrnis@gmail.com>
2021-10-14 15:35:21 +0900

    함수 구조를 interface로 선언하는 방법

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,30 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 const seho: User = {
   age: 10,
   name: '세호',
+};
+
+// 함수에 인터페이스 활용
+function getUser(user: User): void {
+  console.log(user);
+}
+
+const charming = {
+  name: '박민규',
+  age: 100,
+};
+
+getUser(charming);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+var sum: SumFunction;
+sum = function(a: number, b: number) {
+  return a + b;
 };
