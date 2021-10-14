5dc350cd3e46fe457ae3dbe60d7e2b2a16be4ceb
Charming (차밍) <loadkrnis@gmail.com>
2021-10-14 11:28:23 +0900

    js, ts 기타 Array, object 타입 선언

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -9,3 +9,21 @@ let num2: number = 123;
 let arr: Array<number> = [1, 2, 3, 4, 5, 6];
 let items: number[] = [1, 2, 3, 4, 5];
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+
+// ts 튜플
+const address: [string, number] = ['hello', 123];
+
+// ts 객체
+let obj: object = {};
+
+class Person {
+  name: string;
+  age: number;
+}
+
+let person: Person = {
+  name: 'charming',
+  age: 100,
+};
+
+let bool: boolean = true;
