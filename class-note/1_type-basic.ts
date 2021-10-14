494ccc17824ccd5b2da9c1225784aa230cd5d752
Charming (차밍) <loadkrnis@gmail.com>
2021-10-14 11:12:02 +0900

    js, ts 프리미티브 타입 선언o

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,11 @@
+// js 문자열 선언
+let str1 = 'hello';
+
+// ts 문자열 선언
+let str2: string = 'hello';
+let num2: number = 123;
+
+// ts 배열
+let arr: Array<number> = [1, 2, 3, 4, 5, 6];
+let items: number[] = [1, 2, 3, 4, 5];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
