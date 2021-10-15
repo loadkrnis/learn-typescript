8959f8491f92f9aa41b8dc6f2fb8447aacc5fc23
Charming (차밍) <loadkrnis@gmail.com>
2021-10-15 13:33:22 +0900

    generic 사용 목적

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -16,3 +16,16 @@ logText2<number>(10);
 logText2<string>('typescript');
 logText2<boolean>(true);
 
+function logText3Text(text: string) {
+  console.log(text);
+  return text;
+}
+
+function logText3Number(text: number) {
+  console.log(text);
+  return text;
+}
+
+// 제네릭을 사용하지 않고 여러 타입에 대응하면 이와같이 코드 중복이 발생한다.
+logText3Text('a');
+logText3Number(123);
