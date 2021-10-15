17989e2da5d9eb861f5d7184b80b8a78f05b1598
Charming (차밍) <loadkrnis@gmail.com>
2021-10-15 14:25:05 +0900

    기존 유니온 방식으로 작성한 ts

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,27 @@
-const emails = [
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
+
+
+const emails: Email[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: ProductNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: Email | ProductNumber) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -19,8 +30,12 @@ function createDropdownItem(item) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
-emails.forEach(function (email) {
+emails.forEach(function(email) {
   const item = createDropdownItem(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(product => {
+  const item = createDropdownItem(product);
 });
