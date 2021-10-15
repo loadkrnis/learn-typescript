c9c6e0f7bf72257f5880e3cbe761e3a689cf6c2c
Charming (차밍) <loadkrnis@gmail.com>
2021-10-15 11:30:22 +0900

    class는 단순히 문법만 바뀐 것 내부 로직은 같다.

--- class-note/class.js
+++ class-note/class.js
@@ -1,3 +1,11 @@
+function Person2(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+let seho2 = new Person2('세호', 30); // 생성
+console.log(seho2); // Person2 {name: '세호', age: 30}
+
 // ES2015 (ES6)
 class Person {
   constructor(name, age) {
