ae52c289d77c8c80b06a33b63d559fd63015baad
Charming (차밍) <loadkrnis@gmail.com>
2021-10-15 14:43:46 +0900

    ts generic 활용

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -8,20 +8,33 @@ interface ProductNumber {
   selected: boolean;
 }
 
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
 
-const emails: Email[] = [
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
+  const option = document.createElement('option');
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+
+// NOTE: createDropdownItem 함수를 유니온 방식이 아닌 제네릭으로 다시 작성해보기
+function createDropdownItem2<T>(item: DropdownItem<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -31,13 +44,13 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function(email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem2<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
 
 numberOfProducts.forEach(product => {
-  const item = createDropdownItem(product);
+  const item = createDropdownItem2<number>(product);
 });
 
 interface Dropdown {
