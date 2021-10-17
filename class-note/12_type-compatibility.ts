72f4f385b3f9862e9c60a5de57f9bf0187e4012a
Charming (차밍) <loadkrnis@gmail.com>
2021-10-17 22:07:27 +0900

    타입 호환 예제 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -13,3 +13,32 @@ var person: Person;
 
 developer = person; // 불가능
 person = developer; // 가능
+
+var add = function(a: number) {
+};
+
+var sum = function(a: number, b: number) {
+};
+
+// 두 함수 중 범위가 더 큰 것이 sum이다. sum을 범위가 작은 add에 대입을 할 수 있다.
+sum = add;
+add = sum;
+
+//제네릭
+interface Empty<T> {
+
+}
+
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+empty1 = empty2; // 가능
+empty2 = empty1; // 가능
+
+interface NotEmpty<T> {
+  data: T;
+}
+
+var notempty1: NotEmpty<string>;
+var notempty2: NotEmpty<number>;
+notempty1 = notempty2; // 불가능 에러
+notempty2 = notempty1; // 불가능 에러
