4b3b45606089e85ebf08f4b9fd9652aca9b3249d
Charming (차밍) <loadkrnis@gmail.com>
2021-10-17 21:37:15 +0900

    타입 단언 as 키워드

--- class-note/10_utility-types.ts
+++ class-note/10_utility-types.ts
@@ -3,3 +3,6 @@ let a; // any
 let b = 10; // number
 let c = a; // any
 let d = a as string; // 타입 단언
+
+const div = document.querySelector('.container') as HTMLDivElement;
+div.textContent = 'hello';
